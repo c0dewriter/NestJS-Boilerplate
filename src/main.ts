f3eaccc1b012fdf6c0d7adca909461b7051bb3d3
Matin ShayanFar <matinshayanfar@pm.me>
2024-05-24 14:26:21 +0330

    Configured compodoc + swagger + i18n + etc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,56 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { VersioningType } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import helmet from "helmet";
+import { I18nValidationExceptionFilter, I18nValidationPipe } from "nestjs-i18n";
+
+import { AppModule } from "./app.module";
+import metadata from "./metadata";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(
+    new I18nValidationExceptionFilter({
+      detailedErrors: false,
+    }),
+  );
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: "v",
+  });
+
+  app.use(helmet());
+
+  app.useGlobalPipes(
+    new I18nValidationPipe({
+      transform: true,
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+      stopAtFirstError: process.env.NODE_ENV !== "development",
+      disableErrorMessages: process.env.NODE_ENV !== "development",
+    }),
+  );
+
+  app.enableCors();
+
+  if (process.env.NODE_ENV === "development") {
+    const openAPIConfig = new DocumentBuilder()
+      .addBearerAuth()
+      .setTitle("Boilerplate")
+      .setDescription("Super Stable and Production-Ready 😬")
+      .setVersion("0.1")
+      .build();
+
+    await SwaggerModule.loadPluginMetadata(metadata);
+
+    const swaggerDocument = SwaggerModule.createDocument(app, openAPIConfig, {
+      ignoreGlobalPrefix: false,
+    });
+    SwaggerModule.setup("/docs/v1/swagger/", app, swaggerDocument);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
