38c8eeadf2c5eeb54e4755fe4f9eb796241520ec
Matin ShayanFar <matinshayanfar@pm.me>
2026-05-06 13:59:58 +0300

    chore(actions): added github actions for automatic versioning + commit linting
    :skip:
    
    BREAKING CHANGE: I lied. They don't break anything. I just wanna see how it works with a "breaking"
    change

---
+++ commitlint.config.mjs
@@ -0,0 +1,152 @@
+const Configuration = {
+	extends: ["@commitlint/config-conventional"],
+	parserPreset: {
+		name: "conventional-changelog-conventionalcommits",
+		presetConfig: {
+			types: [
+				{ type: "feat", section: "Features" },
+				{ type: "fix", section: "Bug Fixes" },
+				{ type: "docs", section: "Documentation", hidden: false },
+			],
+		},
+	},
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"ci", // Changes to CI/CD configuration and scripts
+				"chore", // Maintenance tasks that don't modify src or test files
+				"docs", // Documentation-only changes
+				"feat", // A new feature
+				"fix", // A bug fix
+				"perf", // A code change that improves performance
+				"refactor", // A code change that neither fixes a bug nor adds a feature
+				"revert", // Reverts a previous commit
+				"style", // Changes that don't affect the meaning of the code (formatting, whitespace, etc.)
+			],
+		],
+		"type-case": [2, "always", "lower-case"],
+		"type-empty": [2, "never"],
+		"scope-case": [2, "always", "lower-case"],
+		"subject-case": [2, "always", "lower-case"],
+		"subject-empty": [2, "never"],
+		"subject-full-stop": [2, "never", "."],
+		"subject-max-length": [2, "always", 72],
+		"header-max-length": [2, "always", 100],
+		"body-leading-blank": [2, "always"],
+		"body-max-line-length": [2, "always", 100],
+		"footer-leading-blank": [2, "always"],
+		"footer-max-line-length": [2, "always", 100],
+	},
+	prompt: {
+		settings: {},
+		messages: {
+			skip: ":skip",
+			max: "upper %d chars",
+			min: "%d chars at least",
+			emptyWarning: "can not be empty",
+			upperLimitWarning: "over limit",
+			lowerLimitWarning: "below limit",
+		},
+		questions: {
+			type: {
+				description: "Select the type of change that you're committing:",
+				enum: {
+					feat: {
+						description: "A new feature",
+						title: "Features",
+						emoji: "✨",
+					},
+					fix: {
+						description: "A bug fix",
+						title: "Bug Fixes",
+						emoji: "🐛",
+					},
+					docs: {
+						description: "Documentation only changes",
+						title: "Documentation",
+						emoji: "📚",
+					},
+					style: {
+						description:
+							"Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
+						title: "Styles",
+						emoji: "💎",
+					},
+					refactor: {
+						description:
+							"A code change that neither fixes a bug nor adds a feature",
+						title: "Code Refactoring",
+						emoji: "📦",
+					},
+					perf: {
+						description: "A code change that improves performance",
+						title: "Performance Improvements",
+						emoji: "🚀",
+					},
+					test: {
+						description: "Adding missing tests or correcting existing tests",
+						title: "Tests",
+						emoji: "🚨",
+					},
+					build: {
+						description:
+							"Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)",
+						title: "Builds",
+						emoji: "🛠",
+					},
+					ci: {
+						description:
+							"Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)",
+						title: "Continuous Integrations",
+						emoji: "⚙️",
+					},
+					chore: {
+						description: "Other changes that don't modify src or test files",
+						title: "Chores",
+						emoji: "♻️",
+					},
+					revert: {
+						description: "Reverts a previous commit",
+						title: "Reverts",
+						emoji: "🗑",
+					},
+				},
+			},
+			scope: {
+				description:
+					"What is the scope of this change (e.g. component or file name)",
+			},
+			subject: {
+				description:
+					"Write a short, imperative tense description of the change",
+			},
+			body: {
+				description: "Provide a longer description of the change",
+			},
+			isBreaking: {
+				description: "Are there any breaking changes?",
+			},
+			breakingBody: {
+				description:
+					"A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself",
+			},
+			breaking: {
+				description: "Describe the breaking changes",
+			},
+			isIssueAffected: {
+				description: "Does this change affect any open issues?",
+			},
+			issuesBody: {
+				description:
+					"If issues are closed, the commit requires a body. Please enter a longer description of the commit itself",
+			},
+			issues: {
+				description: 'Add issue references (e.g. "fix #123", "re #123".)',
+			},
+		},
+	},
+};
+
+export default Configuration;
